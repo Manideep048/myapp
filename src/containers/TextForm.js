45871000069e892509c79f25fa47e0af60f32899
Manideep <211fa04630@vignan.ac.in>
2024-06-19 08:45:58 +0530

    added few logics

--- src/containers/TextForm.js
+++ src/containers/TextForm.js
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function TextForm(props) {
+
+
+  const handleUpClick = () =>{
+
+    // console.log("Uppercase was clicked: " + text);
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+
+  const handleBelowClick = () =>{
+
+    // console.log("LowerCase was clicked: " + text);
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
+
+  const handleOnChange = (event) =>{
+
+    // console.log("On Change");
+    setText(event.target.value);
+
+  }
+
+  const [text, setText] = useState("Enter Text here");
+
   return (
-    <div>
+    <>
+    <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary">Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleBelowClick}>Convert to LowerCase</button>
     </div>
+
+    <div className="container my-2">
+      <h2>Text Summary</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes to read</p>
+      <h3>Preview Text</h3>
+      <p>{text}</p>
+    </div>
+
+    </>
   )
 }
